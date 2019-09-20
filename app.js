44a7e849175b5cf3ad345bf4fd8ccfe0b1953d7e
JodiBonJodi <joedvice@gmail.com>
2019-09-20 10:09:19 -0700

    reworked madlibs for new story

--- app.js
+++ app.js
@@ -1,18 +1,94 @@
 function madLib() {        
-    const nounInput = document.getElementById('noun-input');
-    const noun = nounInput.value;
-    const nounSpan = document.getElementById('noun');
-    nounSpan.textContent = noun;
+    const noun1Input = document.getElementById('noun1-input');
+    const noun1 = noun1Input.value;
+    const noun1Span = document.getElementById('noun1');
+    noun1Span.textContent = noun1;
 
-    const verbInput = document.getElementById('verb-input');
-    const verb = verbInput.value;
-    const verbSpan = document.getElementById('verb');
-    verbSpan.textContent = verb;
+    
+    //const noun2Input = document.getElementById('noun2-input');
+    //const noun2 = noun2Input.value;
+    //const noun2Span = document.getElementById('noun2');
+    //noun2Span.textContent = noun2;
 
-    const adjInput = document.getElementById('adj-input');
-    const adj = adjInput.value;
-    const adjSpan = document.getElementById('adj');
-    adjSpan.textContent = adj;
+    const verb1Input = document.getElementById('verb1-input');
+    const verb1 = verb1Input.value;
+    const verb1Span = document.getElementById('verb1');
+    verb1Span.textContent = verb1;
+
+    const noun3Input = document.getElementById('noun3-input');
+    const noun3 = noun3Input.value;
+    const noun3Span = document.getElementById('noun3');
+    noun3Span.textContent = noun3;
+
+    const adverb1Input = document.getElementById('adverb1-input');
+    const adverb1 = adverb1Input.value;
+    const adverb1Span = document.getElementById('adverb1');
+    adverb1Span.textContent = adverb1;
+
+    const noun4Input = document.getElementById('noun4-input');
+    const noun4 = noun4Input.value;
+    const noun4Span = document.getElementById('noun4');
+    noun4Span.textContent = noun4;
+
+    const noun5Input = document.getElementById('noun5-input');
+    const noun5 = noun5Input.value;
+    const noun5Span = document.getElementById('noun5');
+    noun5Span.textContent = noun5;
+
+    const profession1Input = document.getElementById('profession1-input');
+    const profession1 = profession1Input.value;
+    const profession1Span = document.getElementById('profession1');
+    profession1Span.textContent = profession1;
+
+    const verb2Input = document.getElementById('verb2-input');
+    const verb2 = verb2Input.value;
+    const verb2Span = document.getElementById('verb2');
+    verb2Span.textContent = verb2;
+
+    const noun6Input = document.getElementById('noun6-input');
+    const noun6 = noun6Input.value;
+    const noun6Span = document.getElementById('noun6');
+    noun6Span.textContent = noun6;
+
+    const verbgerund1Input = document.getElementById('verbgerund1-input');
+    const verbgerund1 = verbgerund1Input.value;
+    const verbgerund1Span = document.getElementById('verbgerund1');
+    verbgerund1Span.textContent = verbgerund1;
+
+    const verb3Input = document.getElementById('verb3-input');
+    const verb3 = verb3Input.value;
+    const verb3Span = document.getElementById('verb3');
+    verb3Span.textContent = verb3;
+
+    const adjective1Input = document.getElementById('adjective1-input');
+    const adjective1 = adjective1Input.value;
+    const adjective1Span = document.getElementById('adjective1');
+    adjective1Span.textContent = adjective1;
+
+    const verb4Input = document.getElementById('verb4-input');
+    const verb4 = verb4Input.value;
+    const verb4Span = document.getElementById('verb4');
+    verb4Span.textContent = verb4;
+
+    const bodypart1Input = document.getElementById('bodypart1-input');
+    const bodypart1 = bodypart1Input.value;
+    const bodypart1Span = document.getElementById('bodypart1');
+    bodypart1Span.textContent = bodypart1;
+
+    const noun7Input = document.getElementById('noun7-input');
+    const noun7 = noun7Input.value;
+    const noun7Span = document.getElementById('noun7');
+    noun7Span.textContent = noun7;
+
+    const profession2Input = document.getElementById('profession2-input');
+    const profession2 = profession2Input.value;
+    const profession2Span = document.getElementById('profession2');
+    profession2Span.textContent = profession2;
+
+    const seasoning1Input = document.getElementById('seasoning1-input');
+    const seasoning1 = seasoning1Input.value;
+    const seasoning1Span = document.getElementById('seasoning1');
+    seasoning1Span.textContent = seasoning1;
 
     const showStory = document.getElementById('story');
     showStory.classList.remove('hidden');
